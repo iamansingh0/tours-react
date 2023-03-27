a703abe07f575f65f0b0d5ecea7508f6afa06337
Aman Kumar Singh <amankumar7355197337@gmail.com>
2023-03-27 23:40:16 +0530

    almost done

--- src/data.js
+++ src/data.js
@@ -39,42 +39,42 @@ export const services = [
 export const tours = [
   {
     id: 1,
-    img: require('./images/tour-1.jpeg'),
-    date: 'august 26th, 2020',
-    title: 'Tibet Adventure',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-    country: 'china',
+    img: require('./images/tour-1.jpg'),
+    date: 'august 26th, 2023',
+    title: 'City of lights',
+    text: 'Bonjour Mon Ami! Paris needs no formal introduction - synonymous with culture, architecture, food and fashion. One of the greatest art repositories in the world, harbouring artistic pedigree in the likes of Renoir, Rodin, Picasso, Monet and Van Gogh.',
+    country: 'France',
     duration: '6 days',
-    price: 'from $2100'
+    price: 'from $3500'
   },
   {
     id: 2,
-    img: require('./images/tour-2.jpeg'),
-    date: 'october 1th, 2020',
-    title: 'best of java',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-    country: 'indonesia',
-    duration: '11 days',
-    price: 'from $1400'
+    img: require('./images/tour-2.jpg'),
+    date: 'october 1th, 2023',
+    title: ' Machu Picchu Ruins',
+    text: 'The Incan ruins of Machu Picchu continue to astound historians, archaeologists and historical enthusiasts alike. A mystery of its real purpose surrounds the place and historians till date continue to debate on the matter.',
+    country: 'peru',
+    duration: '10 days',
+    price: 'from $2000'
   },
   {
     id: 3,
     img: require('./images/tour-3.jpeg'),
-    date: 'september 15th, 2020',
-    title: 'explore hong kong',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-    country: 'hong kong',
+    date: 'september 15th, 2023',
+    title: 'new zealand adventures',
+    text: 'New Zealand lies in the Pacific Ocean, to the southwest. New Zealand promises beautiful landscapes and serene, equally beautiful coastlines. Rich in Maori culture is a diverse mix that will take you back in time to New Zealands past.',
+    country: 'new zealand',
     duration: '8 days',
     price: 'from $5000'
   },
   {
     id: 4,
-    img: require('./images/tour-4.jpeg'),
-    date: 'december 5th, 2019',
-    title: 'kenya highlights',
-    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
-    country: 'kenya',
-    duration: '20 days',
+    img: require('./images/tour-4.jpg'),
+    date: 'december 5th, 2023',
+    title: 'breathtaking manali',
+    text: 'With spectacular valleys, breathtaking views, snowcapped mountains, and lush forests of oak, deodar, and pine, Manali is a magical hill station at the northern end of Kullu valley in Himachal Pradesh.',
+    country: 'India',
+    duration: '5 days',
     price: 'from $3300'
   },
 ]
