0e3fa5bccf463bc1f0b6e2f7a71e817c2d32c24c
Aman Kumar Singh <amankumar7355197337@gmail.com>
2023-03-27 22:23:59 +0530

    tours updated

--- src/data.js
+++ src/data.js
@@ -1,14 +1,18 @@
 export const pageLinks = [
-    {id: 1, href: '#home', text: 'home'},
-    {id: 2, href: '#about', text: 'about'},
-    {id: 3, href: '#services', text: 'services'},
-    {id: 4, href: '#tours', text: 'tours'},
+  { id: 1, href: '#home', text: 'home' },
+  { id: 2, href: '#about', text: 'about' },
+  { id: 3, href: '#services', text: 'services' },
+  { id: 4, href: '#tours', text: 'tours' },
 ]
 
 export const socialLinks = [
-    {id: 1, href: 'https://www.instagram.com/iaman._1/', icon: 'fab fa-instagram'},
-    {id: 2, href: 'https://twitter.com/dank_aman', icon: 'fab fa-twitter'},
-    {id: 3, href: 'https://github.com/iamansingh0', icon: 'fab fa-github'},
+  {
+    id: 1,
+    href: 'https://www.instagram.com/iaman._1/',
+    icon: 'fab fa-instagram',
+  },
+  { id: 2, href: 'https://twitter.com/dank_aman', icon: 'fab fa-twitter' },
+  { id: 3, href: 'https://github.com/iamansingh0', icon: 'fab fa-github' },
 ]
 
 // export const titles = [
@@ -18,7 +22,65 @@ export const socialLinks = [
 // ]
 
 export const services = [
-    {id: 1, icon: 'fas fa-wallet fa-fw', title: 'saving money', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
-    {id: 2, icon: 'fas fa-tree fa-fw', title: 'endless hiking', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
-    {id: 3, icon: 'fas fa-socks fa-fw', title: 'amazing comfort', text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.'},
+  {
+    id: 1,
+    icon: 'fas fa-wallet fa-fw',
+    title: 'saving money',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 2,
+    icon: 'fas fa-tree fa-fw',
+    title: 'endless hiking',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+  {
+    id: 3,
+    icon: 'fas fa-socks fa-fw',
+    title: 'amazing comfort',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+]
+
+export const tours = [
+  {
+    id: 1,
+    img: './images/tour-1.jpeg',
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'china',
+    duration: '6 days',
+    price: 'from $2100'
+  },
+  {
+    id: 2,
+    img: './images/tour-2.jpeg',
+    date: 'october 1th, 2020',
+    title: 'best of java',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'indonesia',
+    duration: '11 days',
+    price: 'from $1400'
+  },
+  {
+    id: 3,
+    img: './images/tour-3.jpeg',
+    date: 'september 15th, 2020',
+    title: 'explore hong kong',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'hong kong',
+    duration: '8 days',
+    price: 'from $5000'
+  },
+  {
+    id: 4,
+    img: './images/tour-4.jpeg',
+    date: 'december 5th, 2019',
+    title: 'kenya highlights',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'kenya',
+    duration: '20 days',
+    price: 'from $3300'
+  },
 ]
